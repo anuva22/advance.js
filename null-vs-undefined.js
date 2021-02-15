6374d834a3b77b9700e8535cfd5a8082c1caa3d1
anuva22 <anuvaanuva22@gmail.com>
2021-02-15 17:00:38 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,24 @@
+//let baby;
+//function add(num1, num2){
+    //console.log(num1 + num2);
+   // return (only return no value is also undefined)
+//}
+//const result = add(13, 57);
+
+// function add (num1, num2){
+//     console.log(num1, num2);
+// }
+//const result = add(13);
+
+// const lover = {name:"smart dude", phone:123455};
+// console.log(lover.gf);
+//console.log(result);
+//console.log(baby);
+
+let fun = undefined;
+console.log(fun);
+//defined but set as undefined for fun
+
+let ages = [2, 3, 4];
+console.log(ages[11]);
+
---
+++ truefalse.js
@@ -0,0 +1,10 @@
+//const age = 4;
+//if (age > 0){
+//const name = "0";
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
